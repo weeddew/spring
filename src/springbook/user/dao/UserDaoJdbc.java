42b4936ef0fdfeb254784dca6d0e84f7973845f4
weedew <weeddew@gmail.com>
2012-09-11 20:03:33 +0900

    commit

--- src/springbook/user/dao/UserDaoJdbc.java
+++ src/springbook/user/dao/UserDaoJdbc.java
@@ -30,44 +30,25 @@ public class UserDaoJdbc implements UserDao {
 			}
 		};
 
-	/* (non-Javadoc)
-	 * @see springbook.user.dao.UserDao#add(springbook.user.domain.User)
-	 */
-	@Override
+
 	public void add(User user) {
 		jdbcTemplate.update("insert into spring_users(id, name, password) values (?, ?, ?)",
 				user.getId(), user.getName(), user.getPassword());
 	}
 
-	/* (non-Javadoc)
-	 * @see springbook.user.dao.UserDao#deleteAll()
-	 */
-	@Override
 	public void deleteAll() {
 		jdbcTemplate.update("delete from spring_users");
 	}
 
-	/* (non-Javadoc)
-	 * @see springbook.user.dao.UserDao#get(java.lang.String)
-	 */
-	@Override
 	public User get(String id) {
 		return jdbcTemplate.queryForObject("select * from spring_users where id = ?",
 				new Object[] {id}, userMapper);
 	}
 
-	/* (non-Javadoc)
-	 * @see springbook.user.dao.UserDao#getAll()
-	 */
-	@Override
 	public List<User> getAll() {
 		return jdbcTemplate.query("select * from spring_users order by id", userMapper);
 	}
 
-	/* (non-Javadoc)
-	 * @see springbook.user.dao.UserDao#getCount()
-	 */
-	@Override
 	public int getCount() {
 		return jdbcTemplate.queryForInt("select count(*) from spring_users");
 	}

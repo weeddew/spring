c412500695e743ebc90ca76fa90578f66cab42f5
weedew <weeddew@gmail.com>
2012-09-11 21:15:48 +0900

    commit

--- src/springbook/user/service/UserService.java
+++ src/springbook/user/service/UserService.java
@@ -1,17 +1,10 @@
 package springbook.user.service;
 
-import java.sql.Connection;
-import java.sql.SQLException;
 import java.util.List;
 
-import javax.sql.DataSource;
-
-import org.springframework.jdbc.datasource.DataSourceTransactionManager;
-import org.springframework.jdbc.datasource.DataSourceUtils;
 import org.springframework.transaction.PlatformTransactionManager;
 import org.springframework.transaction.TransactionStatus;
 import org.springframework.transaction.support.DefaultTransactionDefinition;
-import org.springframework.transaction.support.TransactionSynchronizationManager;
 
 import springbook.user.dao.UserDao;
 import springbook.user.domain.User;
@@ -19,21 +12,19 @@ import springbook.user.domain.User;
 public class UserService {
 
 	private UserDao userDao;
-	private DataSource dataSource;
+	private PlatformTransactionManager transactionManager;
 
 	public void setUserDao(UserDao userDao) {
 		this.userDao = userDao;
 	}
 
-	public void setDataSource(DataSource dataSource) {
-		this.dataSource = dataSource;
+	public void setTransactionManager(PlatformTransactionManager transactionManager) {
+		this.transactionManager = transactionManager;
 	}
-
 
 
 	public void addUsers(List<User> users) throws Exception {
 
-		PlatformTransactionManager transactionManager = new DataSourceTransactionManager(dataSource);
 		TransactionStatus status = transactionManager.getTransaction(new DefaultTransactionDefinition());
 
 		try {

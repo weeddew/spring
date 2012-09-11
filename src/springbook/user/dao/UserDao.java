0eea071bf0424c10d5e8eee4dd48f42f948159d7
weedew <weeddew@gmail.com>
2012-09-11 15:58:34 +0900

    commit

--- src/springbook/user/dao/UserDao.java
+++ src/springbook/user/dao/UserDao.java
@@ -27,6 +27,13 @@ public class UserDao {
 
 	public void deleteAll() throws SQLException {
 
+		class DeleteAllStatement implements StatementStrategy {
+			public PreparedStatement makePreparedStatement(Connection c) throws SQLException {
+				PreparedStatement ps = c.prepareStatement("delete from spring_users");
+				return ps;
+			}
+		}
+
 		StatementStrategy strategy = new DeleteAllStatement();
 		jcbcContext(strategy);
 	}
